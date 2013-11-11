519cae98f672e935613854a8dab9a2c9e2124a77
Takashi Kokubun <takashikkbn@gmail.com>
2013-11-11 20:19:04 +0900

    Fix games score calculation

--- app/models/game/total.rb
+++ app/models/game/total.rb
@@ -16,24 +16,36 @@ class Game::Total < ActiveRecord::Base
   validates :game_id, presence: true
   validates :total_score, presence: true
 
-  # x = 1/1 + 1/2 + 1/3 + ... + 1/n
-  # (150/x + A)/1 , (150/x + A)/2 , (150/x + A)/3 ...
+  def self.initialize_point
+    Game::Total.all.each do |game_total|
+      game_total.point = 0
+      game_total.save
+    end
+  end
+
+  # n人中x位のスコアf(x)を計算する。
+  # プレー者に反比例で合計150点を与える。
+  # A = POINT_PARAMETER
+  # Σ = 1/(1 + A) + 1/(2 + A) + ... + 1/(n + A)
+  # f(x) = (150/Σ)/(x + A)
   def self.update_point
+    self.initialize_point
     ::Game.all.each do |game|
-      game_totals = Game::Total.where(game_id: game.id).order('total_score DESC')
+      game_totals = Game::Total.where(game_id: game.id).where('total_score > 0').order('total_score DESC')
       user_count = game_totals.count
       sigma = self.sigma(user_count)
       game_totals.each_with_index do |game_total, index|
-        game_total.point = ((150.0 / sigma + POINT_PARAMETER) / (index + 1)).to_i
+        rank = index + 1
+        game_total.point = ((150.0 / sigma) / (rank + POINT_PARAMETER)).to_i
         game_total.save
       end
     end
   end
 
-  # 1/1 + 1/2 + 1/3 + ... + 1/n
+  # Σ = 1/(1 + A) + 1/(2 + A) + ... + 1/(n + A)
   def self.sigma(n)
     1.upto(n).inject(0) do |sigma, k|
-      sigma + 1.0 / k
+      sigma + 1.0 / (k + POINT_PARAMETER)
     end
   end
 end
