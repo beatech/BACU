12cd6a6c2f99e13e714c7b9ef7b9c5abac43e5ef
Takashi Kokubun <takashikkbn@gmail.com>
2013-12-30 14:36:29 +0900

    Disable asset logging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,18 @@
+class DisableAssetsLogger
+  def initialize(app)
+    @app = app
+    Rails.application.assets.logger = Logger.new('/dev/null')
+  end
+
+  def call(env)
+    previous_level = Rails.logger.level
+    Rails.logger.level = Logger::ERROR if env['PATH_INFO'].index('/assets/') == 0
+    @app.call(env)
+  ensure
+    Rails.logger.level = previous_level
+  end
+end
+
 Bacu::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -26,4 +41,7 @@ Bacu::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Disable assets logging
+  config.middleware.insert_before Rails::Rack::Logger, DisableAssetsLogger
 end
